ef2f5b20d69fe61da9b18d72bd55254b7c4ea473
Ackweld <Axel-bj@hotmail.com>
2023-12-05 13:38:52 +0100

    comments

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,3 +1,5 @@
+// Ssed for getting the final digits of the character url so that it can be used
+// as an id... Since SWAPI characters don't have an id for some reason..
 export const extractNumberFromString = (input: string): number | null => {
   const match = input.match(/(\d+)\/?$/);
 
